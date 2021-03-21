5eb2985f3ea15341ed64a552e7fe69b5439cff38
Saada <no@email.com>
2021-03-21 22:26:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# ReportCrypto
+
+
+## How To Use ##
+
+1. Clone the repository
+```bash
+git clone https://github.com/adel-saada/ReportCrypto.git
+```
+2. Access to the folder
+```bash
+cd ReportCrypto/
+```
+
+3. Install VirtualEnvironment
+
+  ```[DEBIAN/UBUNTU] ``` 
+  ```bash
+  apt-get install python3-venv 
+  ```
+
+  ```[CENTOS/REDHAT]```
+  ```bash
+  yum install python3-venv 
+  ```
+
+
+4. Create your env
+```bash
+python3 -m venv myenv
+```
+
+5. Activate your env
+```bash
+source myenv/bin/activate
+```
+
+6. Install all requirement (package python for use script)
+```bash
+python -m pip install -r requirement.txt
+```
+
+7. Create your file '.env' which contains credential gmail
+```bash
+vim .env
+```
+```
+GMAIL_PASSWORD='<your_gmail_password>'
+GMAIL_ADRESS='<your_gmail_adress>'
+```
+8. Edit file possession.csv for put respectively the name of the crypto, the number of currency and the total invested
+
+- Separator is '|'  (ALTGR + 6)
+- The exact name of the crypto (you can complete with a number if you had invest for the same crypto but with different value, like bitcoin1 bitcoin2 bitcoin3000..)
+- You can add others crypto than in the example below of course, but just write the exact name of crypto
+- Structure is : "exact_name_crypto"(string)|total_currency_of_this_crypto(float)|total_invest(int)
+
+```EXAMPLE : ```
+```
+"bitcoin"|0.0041631|12
+"ethereum"|0.000532081|11
+"ethereum2"|0.0012244117|10
+"chainlink"|1.86015018|14
+"chainlink2"|1.09068143|18
+"hedera-hashgraph"|100.589|1
+"theta-network"|5.4435|12
+"vechain"|200|10
+"oasis-network"|12.699|10
+"pundi-x"|0.127|10
+```
+
+9. Run script
+```bash
+./infos_crypto.py
+```
+
+10. [OPTIONAL] Add Crontab
+
+```bash
+crontab -e 
+```
+```
+#Script Crypto Informations
+SHELL=/bin/bash
+0 9,21 * * * cd /home/your/path/report_crypto/ && ./botenv/bin/python3 ./infos_crypto.py
+```
---
+++ infos_crypto.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+
+from pycoingecko import CoinGeckoAPI
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from collections import OrderedDict
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+# Variables log 
+GMAIL_PASSWORD = os.environ.get('GMAIL_PASSWORD')
+GMAIL_ADRESS = os.environ.get('GMAIL_ADRESS')
+PORT=587
+
+import csv
+
+# dict possessions crypto (nombre de pièce et cout investissement)
+
+POSSESSIONS={}
+
+with open('possession.csv','r') as file:
+    reader = csv.reader(file, delimiter= '|')
+    for row in reader:
+        POSSESSIONS[row[0]] = [float(row[1]),int(row[2])]
+
+# Objectif : Connection à l'API et retour des prix des crypto avec la devise euros (dict)
+def dict_crypto():
+    cg = CoinGeckoAPI()
+    list_crypto = [key for key in POSSESSIONS.keys()]
+    return cg.get_price(ids=list_crypto, vs_currencies='eur')
+
+
+# Tri dictionnaire pour garder un ordre
+cryptos = OrderedDict(sorted(dict_crypto().items(),key=lambda t: t[0]))
+
+contents = ""
+total_benefices = float(0)
+
+# Calcul montant_total, benefices... et remplissage code html
+for key,values in cryptos.items():
+    name = key
+    value = float(values["eur"])
+    for k,v in POSSESSIONS.items():
+        if name in k:
+            pieces = v[0]
+            cout = v[1]
+            montant_total = value*pieces
+            benefices = montant_total-cout
+            total_benefices += round(benefices,2)
+            
+            contents += "<tr>"
+            contents += "<td> {} </td>".format(name)
+            contents += "<td> {} </td>".format(round(pieces,2))
+            contents += "<td> {} </td>".format(round(cout,2))
+            contents += "<td> {} </td>".format(round(value,4))
+            contents += "<td> {} </td>".format(round(montant_total,2))
+            contents += "<td> {} </td>".format(round(benefices,2))
+            contents += "</tr>"
+
+
+html_str="""
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Crypto</title>
+  <style type="text/css">
+      table {{
+        background: white;
+        border-radius:3px;
+        border-collapse: collapse;
+        height: auto;
+        max-width: 900px;
+        padding:5px;
+        width: 100%;
+        animation: float 5s infinite;
+      }}
+      th {{
+        color:#D5DDE5;;
+        background:#1b1e24;
+        border-bottom: 4px solid #9ea7af;
+        font-size:14px;
+        font-weight: 300;
+        padding:10px;
+        text-align:center;
+        vertical-align:middle;
+      }}
+      tr {{
+        border-top: 1px solid #C1C3D1;
+        border-bottom: 1px solid #C1C3D1;
+        border-left: 1px solid #C1C3D1;
+        color:#666B85;
+        font-size:16px;
+        font-weight:normal;
+      }}
+      tr:hover td {{
+        background:#4E5066;
+        color:#FFFFFF;
+        border-top: 1px solid #22262e;
+      }}
+      td {{
+        background:#FFFFFF;
+        padding:10px;
+        text-align:left;
+        vertical-align:middle;
+        font-weight:300;
+        font-size:13px;
+        border-right: 1px solid #C1C3D1;
+      }}
+    </style>
+</head>
+<body>
+  <table>
+     <thead>
+        <tr> <th colspan="6"> Table des cryptos </th> </tr>
+     </thead>
+     <tbody>
+        <tr>
+           <td>Crypto</td>
+           <td>coins</td>
+           <td>Invest</td>
+           <td>unité €</td>
+           <td>Total</td>
+           <td>Benefices</td>
+        </tr>
+
+        {}
+
+        <tr>
+           <td colspan="5">Total Benefices :</td>
+           <td>{}</td>
+        </tr>
+     </tbody>
+  </table>
+</body>
+</html>
+""".format(contents,total_benefices)
+
+
+
+def mail(html_str):
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "[CRYPTO] - Report Journalier"
+    message["From"] = "myserver"
+    message["To"] = GMAIL_ADRESS
+
+    # Convert both parts to MIMEText objects and add them to the MIMEMultipart message
+    message.attach(MIMEText(html_str,'html') )
+
+    # Send email
+    with smtplib.SMTP('smtp.gmail.com',587) as server:
+        server.starttls()
+        server.login(GMAIL_ADRESS,GMAIL_PASSWORD)
+        server.sendmail(GMAIL_ADRESS,GMAIL_ADRESS,message.as_string() )
+
+
+
+if __name__ == '__main__':
+   mail(html_str) 
